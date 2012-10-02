0326643adae44b50724817b2fa6bd6051c72e8a9
Brij Bhasin <brijbhasin@gmail.com>
2012-10-02 16:37:43 +0530

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GsfCircles::Application.config.secret_token = 'bcb72bcc2019afab49855467c6016dfddb5672dc533dcbb4f95687518c86e96748a26c5815c0e29cc58efa529033f112b35e491c32681e458b31951e1453c325'
